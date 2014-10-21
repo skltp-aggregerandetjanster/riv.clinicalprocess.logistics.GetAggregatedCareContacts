070432be0bf3ca7f5b4da1a7c3bcd8e0734831a0
Hans Thunberg <hans.thunberg@callistaenterprise.se>
2014-10-21 14:00:44 +0200

    Moved teststub  project to the same parent pom.xml

---
+++ GetAggregatedCareContacts-teststub/src/test/java/se/skl/tp/aggregatingservices/clinicalprocess/logistics/getaggregatedcarecontact/GetAggregatedCareContactTeststubMuleServer.java
@@ -0,0 +1,48 @@
+package se.skl.tp.aggregatingservices.clinicalprocess.logistics.getaggregatedcarecontact;
+
+ 
+import org.soitoolkit.commons.mule.test.StandaloneMuleServer;
+
+import org.soitoolkit.commons.mule.util.RecursiveResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class GetAggregatedCareContactTeststubMuleServer {
+
+
+	public static final String MULE_SERVER_ID   = "GetAggregatedCareContact-teststub";
+ 
+
+	private static final Logger logger = LoggerFactory.getLogger(GetAggregatedCareContactTeststubMuleServer.class);
+    private static final RecursiveResourceBundle rb = new RecursiveResourceBundle("GetAggregatedCareContact-config");
+
+	public static void main(String[] args) throws Exception {
+
+ 
+        // Configure the mule-server
+        // Note: do not activate the "soitoolkit-teststubs" profile here since it sets a
+        // system property, which is JVM global and may activate all teststubs in a Mule-instance.
+        // Teststubs are always loaded by the top-level config.xml file.
+        StandaloneMuleServer muleServer = new StandaloneMuleServer(MULE_SERVER_ID, false, true);
+ 
+        // Start the server
+		muleServer.run();
+	}
+
+    /**
+     * Address based on usage of the servlet-transport and a config-property for the URI-part
+     * 
+     * @param serviceUrlPropertyName
+     * @return
+     */
+    public static String getAddress(String serviceUrlPropertyName) {
+
+        String url = rb.getString(serviceUrlPropertyName);
+
+	    logger.info("URL: {}", url);
+    	return url;
+ 
+    }	
+}
