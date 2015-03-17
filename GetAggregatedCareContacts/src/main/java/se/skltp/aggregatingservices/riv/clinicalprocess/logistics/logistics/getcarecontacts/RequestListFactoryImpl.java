80df7d8a8eee99018f465c9832d220becd91c00e
Martin Flower <martin.flower@callistaenterprise.se>
2015-03-17 13:43:27 +0100

    SERVICE-147 - städat innan v1.1.2

---
+++ GetAggregatedCareContacts/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getcarecontacts/RequestListFactoryImpl.java
@@ -0,0 +1,128 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.logistics.logistics.getcarecontacts;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soitoolkit.commons.mule.util.ThreadSafeSimpleDateFormat;
+
+import riv.clinicalprocess.logistics.logistics.getcarecontactsresponder.v2.GetCareContactsType;
+import se.skltp.agp.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentResponseType;
+import se.skltp.agp.riv.itintegration.engagementindex.v1.EngagementType;
+import se.skltp.agp.service.api.QueryObject;
+import se.skltp.agp.service.api.RequestListFactory;
+
+public class RequestListFactoryImpl implements RequestListFactory {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestListFactoryImpl.class);
+    private static final ThreadSafeSimpleDateFormat dtf = new ThreadSafeSimpleDateFormat("yyyyMMddHHmmss");
+    private static final ThreadSafeSimpleDateFormat df = new ThreadSafeSimpleDateFormat("yyyyMMdd");
+
+    /**
+     * Filtrera svarsposter från i EI (ei-engagement) baserat parametrar i GetCareContact requestet (req).
+     * Följande villkor måste vara sanna för att en svarspost från EI skall tas med i svaret:
+     *
+     * 1. req.timePeriod.start <= ei-engagement.mostRecentContent <= req.timePeriod.end
+     *
+     *
+     * Ett anrop görs per funnet sourceSystem med följande värden i anropet:
+     *
+     * 1. logicalAddress = sourceSystem (systemadressering)
+     * 2. subjectOfCareId = orginal-request.subjectOfCareId
+     * 3. careUnitId = orginal-request.careUnitId
+     * 4. fromDate = orginal-request.fromDate
+     * 5. toDate = orginal-request.toDate
+     */
+    @Override
+    public List<Object[]> createRequestList(QueryObject qo, FindContentResponseType src) {
+
+        GetCareContactsType originalRequest = (GetCareContactsType)qo.getExtraArg();
+        Date reqFrom = null;
+        Date reqTo = null;
+
+        if(originalRequest.getTimePeriod() != null){
+            reqFrom = parseTs(originalRequest.getTimePeriod().getStart());
+            reqTo   = parseTs(originalRequest.getTimePeriod().getEnd());
+        }
+
+        FindContentResponseType eiResp = (FindContentResponseType)src;
+        List<EngagementType> inEngagements = eiResp.getEngagement();
+
+        log.info("Got {} hits in the engagement index", inEngagements.size());
+
+        Set<String> sourceSystems = new HashSet<String>();
+
+        for (EngagementType inEng : inEngagements) {
+
+            // Filter
+            if (isBetween(reqFrom, reqTo, inEng.getMostRecentContent())) {
+
+                log.info("Add SS: {}", inEng.getSourceSystem());
+                // Add source system
+                sourceSystems.add(inEng.getSourceSystem());
+            }
+        }
+
+        // Prepare the result of the transformation as a list of request-payloads,
+        // one payload for each unique logical-address (e.g. source system since we are using systemaddressing),
+        // each payload built up as an object-array according to the JAX-WS signature for the method in the service interface
+        List<Object[]> reqList = new ArrayList<Object[]>();
+
+        for (String sourceSystem: sourceSystems) {
+
+            log.info("Calling source system using logical address {} for subject of care id {}", sourceSystem, originalRequest.getPatientId().getId());
+
+            GetCareContactsType request = new GetCareContactsType();
+            request.setPatientId(originalRequest.getPatientId());
+
+            if(originalRequest.getCareUnitHSAId() != null && originalRequest.getCareUnitHSAId().size() > 0){
+                request.getCareUnitHSAId().addAll(originalRequest.getCareUnitHSAId());
+            }
+
+            request.setTimePeriod(originalRequest.getTimePeriod());
+
+            Object[] reqArr = new Object[] {sourceSystem, request};
+
+            reqList.add(reqArr);
+        }
+
+        log.debug("Transformed payload: {}", reqList);
+
+        return reqList;
+    }
+
+    protected Date parseTs(String ts) {
+        try {
+            if (ts == null || ts.length() == 0) {
+                return null;
+            } else {
+                return df.parse(ts);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    // Not in use in this service domain
+    protected boolean isBetween(Date from, Date to, String tsStr) {
+        return true;
+    }
+
+/*    protected boolean isBetween(Date from, Date to, String tsStr) {
+        try {
+            log.debug("Is {} between {} and {}", new Object[]{tsStr, from, to});
+            Date ts = dtf.parse(tsStr);
+            if (from != null && from.after(ts)) return false;
+            if (to != null && to.before(ts)) return false;
+            return true;
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+}
---
+++ GetAggregatedCareContacts/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getcarecontacts/ResponseListFactoryImplTest.java
@@ -0,0 +1,78 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.logistics.logistics.getcarecontacts;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.soitoolkit.commons.mule.jaxb.JaxbUtil;
+
+import riv.clinicalprocess.logistics.logistics.getcarecontactsresponder.v2.GetCareContactsResponseType;
+import riv.clinicalprocess.logistics.logistics.v2.CareContactType;
+import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
+import se.skltp.agp.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentType;
+import se.skltp.agp.service.api.QueryObject;
+
+public class ResponseListFactoryImplTest {
+
+    private static final JaxbUtil jaxbUtil = new JaxbUtil(GetCareContactsResponseType.class, ProcessingStatusType.class);
+
+    @Test
+    public void getXmlFromAggregatedResponse(){
+        FindContentType fc = new FindContentType();
+        fc.setRegisteredResidentIdentification("1212121212");
+        QueryObject queryObject = new QueryObject(fc, null);
+        List<Object> responseList = new ArrayList<Object>(2);
+        responseList.add(createGetCareContactsResponse());
+        responseList.add(createGetCareContactsResponse());
+        ResponseListFactoryImpl responseListFactory = new ResponseListFactoryImpl();
+
+        String responseXML = responseListFactory.getXmlFromAggregatedResponse(queryObject, responseList);
+        GetCareContactsResponseType response = (GetCareContactsResponseType)jaxbUtil.unmarshal(responseXML);
+        assertEquals(2, response.getCareContact().size());
+    }
+
+    private GetCareContactsResponseType createGetCareContactsResponse(){
+        GetCareContactsResponseType getCareDocResponse = new GetCareContactsResponseType();
+        getCareDocResponse.getCareContact().add(new CareContactType());
+        return getCareDocResponse;
+    }
+
+
+    @Test
+    public void getXmlFromAggregatedResponse_incorrect() throws Exception{
+
+        InputStream stream = ResponseListFactoryImplTest.class.getResourceAsStream("/GetCareContact-example-incorrect.xml");
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        GetCareContactsResponseType response = (GetCareContactsResponseType)jaxbUtil.unmarshal(sb.toString());
+        assertEquals(0, response.getCareContact().size());
+    }
+
+    @Test
+    public void getXmlFromAggregatedResponse_correct() throws Exception{
+
+        InputStream stream = ResponseListFactoryImplTest.class.getResourceAsStream("/GetCareContact-example-correct.xml");
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        GetCareContactsResponseType response = (GetCareContactsResponseType)jaxbUtil.unmarshal(sb.toString());
+        assertEquals(1, response.getCareContact().size());
+    }
+}
