1fd0530ac2b75658db031557d61b3051f63c1430
Martin Flower <martin.flower@callistaenterprise.se>
2016-01-15 16:18:32 +0100

    Assertion fel mot Mule 3.7 - work in progress

--- test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcarecontacts/CommonParameters.scala
+++ test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcarecontacts/CommonParameters.scala
@@ -2,9 +2,10 @@ package se.skltp.aggregatingservices.riv.clinicalprocess.logistics.logistics.get
 
 trait CommonParameters {
   val serviceName:String     = "CareContacts"
-  val urn:String             = "urn:riv:clinicalprocess:logistics:logistics:GetCareContactsResponder:2"
+  val urn:String             = "urn:riv:ehr:patientsummary:GetCareContactsResponder:2"
   val responseElement:String = "GetCareContactsResponse"
   val responseItem:String    = "careContact"
+  val responseItemUrn:String = "urn:riv:clinicalprocess:logistics:logistics:GetCareContactsResponder:2"
   var baseUrl:String         = if (System.getProperty("baseUrl") != null && !System.getProperty("baseUrl").isEmpty()) {
                                    System.getProperty("baseUrl")
                                } else {
